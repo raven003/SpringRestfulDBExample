559bc1eb53a0b3f33395c0b66fba037db7c24429
raven003 <xzardos@web.de>
2021-08-14 17:29:16 +0200

    folder strcuture changed

--- src/main/java/com/restful/api/h2/restful/api/h2/Entity/Repository/ProjectRepo.java
+++
@@ -1,8 +0,0 @@
-package com.restful.api.h2.restful.api.h2.Entity.Repository;
-
-import com.restful.api.h2.restful.api.h2.Entity.Project;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectRepo extends CrudRepository<Project,Long> {
-
-}
